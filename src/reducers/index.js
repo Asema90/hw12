a8536b6892115dbbd41eaf960f5da3f2dc82e9ed
Asema90 <66380244+Asema90@users.noreply.github.com>
2023-03-03 20:43:13 +0600

    redux

---
+++ src/reducers/index.js
@@ -0,0 +1,30 @@
+import products from "../api/products.json";
+
+const initialState = { products };
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        products: state.products.map((product) => {
+          if (product.id === action.payload) {
+            return {
+              ...product,
+              inCart: product.inCart + 1,
+              inventory: product.inventory - 1,
+            };
+          } else return product;
+        }),
+      };
+    case "CHECKOUT":
+      return {
+        ...state,
+        products: state.products.map((product) => {
+          return { ...product, inCart: 0 };
+        }),
+      };
+    default:
+      return state;
+  }
+};
